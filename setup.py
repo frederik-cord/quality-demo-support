ea57b4cd088441eb684706971781bbb515f45ba9
Frederik Hvilshøj <frederik@cord.tech>
2022-02-08 08:53:56 +0100

    New dependencies - may not be backward compatible.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="quality-demo-support",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25",
         "urllib3>=1.24",
         "PILLOW>=9.0.0",
         "numpy>=1.21.0",
-        "cord-client-python @ git+https://github.com/cord-team/cord-client-python.git@rp/floaty-tqdm",
-        "cord-dataset @ git+https://github.com/cord-team/cord-dataset.git",
+        "cord-client-python @ git+https://github.com/encord-team/encord-client-python.git@rp/floaty-tqdm",
+        "cord-dataset @ git+https://github.com/encord-team/encord-dataset.git",
         "tqdm",
         "matplotlib>=3.5.1",
         "pandas>=1.1",
